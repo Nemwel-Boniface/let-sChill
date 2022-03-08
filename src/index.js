7bfea39a95177ad4e5f7adb5d4fee858579d3e6a
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-03-08 16:13:55 +0100

    Modified display page
    -modifed display page in index.js to uinique id of the cards

--- src/index.js
+++ src/index.js
@@ -5,26 +5,50 @@ const url = 'https://api.tvmaze.com/shows/';
 const movieWrapper = document.querySelector('.image-container');
 const testMovie = async (url) => {
   movieWrapper.innerHTML = '';
-  for (let i = 720; i < 729; i += 1) {
+  for (let i = 20; i < 29; i += 1) {
     fetch(url + i)
       .then((response) => response.json())
       .then((result) => {
-        movieWrapper.innerHTML = `${movieWrapper.innerHTML}
-        <div class="movie" id="${i}">
-          <img src="${result.image.medium}" alt="movie">
-          <div class="movieDesc">
-            <div class="movieDescTop">
-              <p>${result.name}</p>
-              <a href="#"><i class="fa fa-heart" aria-hidden="true"></i>
-              </a>
-            </div>
-            <p><span>2</span>Likes</p>
-          </div>
-          <div class="movieBtn">
-            <button id="${i}" class="comments" type="button">Comments</button>
-            <button class="reservations" type="button">Reservations</button>
-          </div>
-        </div>`;
+        const movie = document.createElement('div');
+        const movieDes = document.createElement('div');
+        const movieDesTop = document.createElement('div');
+        const movieBtn = document.createElement('div');
+        const likesP = document.createElement('p');
+        const likeSpan = document.createElement('span');
+
+        const like = document.createElement('a');
+        const icon = document.createElement('i');
+        icon.classList.add('fa', 'fa-heart');
+
+        likesP.textContent = 'Likes';
+        likeSpan.textContent = 2;
+        likesP.append(likeSpan, 'like');
+        like.append(icon, likeSpan);
+
+        movie.classList.add('movie');
+        movieDes.classList.add('movieDesc');
+        movieDesTop.classList.add('movieDescTop');
+
+        movie.id = i;
+        const img = document.createElement('img');
+        const title = document.createElement('h3');
+        const genere = document.createElement('p');
+        const summary = document.createElement('p');
+        const comment = document.createElement('button');
+        const reservation = document.createElement('button');
+        title.textContent = result.name;
+        genere.textContent = result.genres;
+        comment.textContent = 'Comment';
+
+        reservation.textContent = 'Reservation';
+
+        movieBtn.append(comment, reservation);
+        img.src = result.image.medium;
+        movieDesTop.append(title, like);
+        movieDes.append(movieDesTop);
+
+        movie.append(img, movieDes, movieBtn);
+        movieWrapper.appendChild(movie);
       });
   }
 };
